17c032146dc50bab9917acb39c5870313ed0211a
lewis-otwori <lewisotwori@gmail.com>
2023-03-12 15:58:54 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+// Write your code 
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "LEWIS is the champion";
